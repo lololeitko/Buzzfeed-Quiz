0753dda00181036a15a4e54f0c9eb7f26658da0f
Glitch (quiz-lindsey-l) <none>
2020-08-12 19:57:58 +0000

    🎌🎳 Checkpoint ./script.js:20007682/242

--- script.js
+++ script.js
@@ -3,13 +3,21 @@
 //#TODO: Create four variables to track each possible quiz outcome
 var questionCount = 0;
 var ISTJ = 0;
-var ISFJ = 0;
-var INTJ = 0;
-var INFJ = 0;
 var ISTP = 0;
+var ISFJ = 0;
 var ISFP = 0;
-var INEP = 0;
 var INFP = 0;
+var INFJ = 0;
+var INTJ = 0;
+var INTP = 0;
+var ESTJ = 0;
+var ESTP = 0;
+var ESFJ = 0;
+var ESFP = 0;
+var ENFP = 0;
+var ENFJ = 0;
+var ENTJ = 0;
+var ENTP = 0;
 
 //#TODO: Use the DOM to create variables for the first quiz question.
 var q1a1 = document.getElementById("q1a1");
